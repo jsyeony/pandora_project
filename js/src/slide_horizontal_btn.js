c07844b2e983b4a3c72a647a7ace933ad8ae9e11
jsyeony <skeo4864@gmail.com>
2022-03-03 00:42:04 +0900

    [update]slide_banner_jQuery

--- js/src/slide_horizontal_btn.js
+++ js/src/slide_horizontal_btn.js
@@ -29,6 +29,83 @@
   var permission = true;
   var i = 0;
   var timed = 1000;
+  var play;
+
+  // 함수
+  var slideGoFn = function(){
+    play = setInterval(function(){
+      nextBtn.trigger('click', timed);
+    }, timed * 3);
+  };
+
+  var slideStopFn = function(){
+    clearInterval(play);
+  };
+
+  var nextBtnFn = function(){
+    if(permission){
+      permission = false;
+      i += 1;
+      if(i >= originSlideLen){
+        slideWrapper.css({'marginLeft' : 100 * i + '%'});
+        i = 0;
+      };
+      slideWrapper.stop().animate({'marginLeft' : -100 * i + '%'}, function(){
+        permission = true;
+      });
+    };
+  };
+
+  var prevBtnFn = function(){
+    if(permission){
+      permission = false;
+      i -= 1;
+      slideWrapper.stop().animate({'marginLeft' : -100 * i + '%'}, function(){
+        if(i < 0){
+          i = originSlideLen - 1;
+          slideWrapper.css({'marginLeft' : -100 * i + '%'});
+        };
+        permission = true;
+      });
+    };
+  };
+
+  var indicatorFn = function(){
+    indiLi.eq(i).addClass('on');
+    indiLi.eq(i).siblings().removeClass('on');
+  };
+
+  // 기능수행
+  var cloneDiv = slideDiv.eq(-1).clone();
+  slideWrapper.prepend(cloneDiv);
+
+  var newSlideDiv = slideWrapper.children('div');
+  var newSlideLen = newSlideDiv.length;
+
+  slideWrapper.css({'width': ( 100 * newSlideLen ) + '%', 'left' : -100 + '%' });
+  newSlideDiv.css({'width' : ( 100 / newSlideLen ) + '%' });
+
+  // slideGoFn();
+
+  // 이벤트 ======================================================================
+  nextBtn.on('click', function(e){
+    e.preventDefault();
+    nextBtnFn();
+    indicatorFn();
+  });
+  prevBtn.on('click', function(e){
+    e.preventDefault();
+    prevBtnFn();
+    indicatorFn();
+  });
+
+  // slideSet.on('mouseenter', function(){
+  //   slideStopFn();
+  // });
+
+  // slideSet.on('mouseleave', function(){
+  //   slideGoFn();
+  // });
 
 
 })(jQuery);
