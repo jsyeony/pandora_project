1e3aa8ca95112ae9b04965976f0b1242f62f2190
jsyeony <skeo4864@gmail.com>
2022-03-07 00:57:52 +0900

    [update]accordion_nav_slideDown&slideUp_add

--- js/src/accordion_nav.js
+++ js/src/accordion_nav.js
@@ -12,38 +12,86 @@
   var gnb = $('.gnb');
   var gnbUl = gnb.children('ul');
   var gnbLi = gnbUl.children('li');
-  var gnbLiLen = gnbLi.length;
+  var gnbLink = gnbLi.children('a');
 
   var accordionArea = gnb.children('.accordion_area');
   var accorWrap = accordionArea.children('.accor_wrap');
 
+  var searchArea = $('.search_area');
+  var searchBtn = searchArea.children('button');
+  var searchInput = searchArea.children('input');
+
   var i = 0;
+  var timed = 1000;
+
+  // 헤더 검색 아이콘 관련 이벤트 수행 ===============================================
+  /*
+    검색 아이콘에 마우스를 올리면 검색창 input이 나타나도록 설정
+    input에서 포커스가 벗어나면 다시 사라지도록 설정
+  */
+  // 기본 수행
+  searchInput.hide();
+
+  // 이벤트
+  searchBtn.on('mouseenter', function(){
+    searchInput.fadeIn(timed/4);
+  });
+
+  searchInput.on('blur', function(){
+    searchInput.fadeOut(timed/3);
+  })
+
+  // 아코디언메뉴를 나타내는 방법 첫번째 : addClass ====================================
+
+  // gnbLi.on('mouseenter', function(){
+  //   i = $(this).index();
+  //   accorWrap.eq(i).addClass('on');
+  //   accorWrap.eq(i).siblings().removeClass('on');
+  // });
+
+  // gnb.on('mouseleave', function(){
+  //   i = $(this).index();
+  //   accorWrap.removeClass('on');
+  // });
+
+  // gnbLink.on('focus', function(){
+  //   i = $(this).parent().index();
+  //   accorWrap.eq(i).addClass('on');
+  //   accorWrap.eq(i).siblings().removeClass('on');
+  // });
   
+  // gnbLink.on('blur', function(){
+  //   accorWrap.removeClass('on');
+  // });
+
+  /* 
+    아코디언 메뉴를 나타내는 방법 두번째 : 먼저 아코디언메뉴를 기본 수행으로 js를 통해
+    숨겨 놓은 뒤 이벤트를 통해 원하는 효과로 나타나도록 지정(slideDown, fadeIn ...)
+  */
+  // 기본 수행
+  accorWrap.hide();
+
   // 이벤트
   gnbLi.on('mouseenter', function(){
     i = $(this).index();
-    accorWrap.eq(i).addClass('on');
-    accorWrap.eq(i).siblings().removeClass('on');
+    accorWrap.eq(i).stop().slideDown(timed/3);
+    accorWrap.eq(i).siblings().stop().hide();
   });
 
   gnb.on('mouseleave', function(){
     i = $(this).index();
-    accorWrap.removeClass('on');
+    accorWrap.stop().slideUp(timed/3);
   });
 
-  // gnbLi.on('mouseenter', function(){
-  //   i = $(this).index();
-  //   accorWrap.eq(i).stop().slideDown(500);
-  //   accorWrap.eq(i).css({'display' : 'flex'});
-  //   accorWrap.eq(i).siblings().css({'display' : 'none'});
-  // })
-
-  // accordionArea.on('mouseleave', function(){
-  //   i = $(this).index()
-  //   accorWrap.eq(i).slideUp();
-  //   accorWrap.eq(i).css({'display':'none'});
-  // });
-
-
+  gnbLink.on('focus', function(){
+    i = $(this).parent().index();
+    accorWrap.eq(i).stop().slideDown(timed/3);
+    accorWrap.eq(i).siblings().stop().hide();
+  });
+  
+  gnbLink.on('blur', function(){
+    accorWrap.stop().slideUp(timed/3);
+  });
+ 
 
 })(jQuery);
