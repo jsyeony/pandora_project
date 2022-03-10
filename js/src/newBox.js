fc417800075f4fe99be13925b9a9f8917350a1d7
jsyeony <skeo4864@gmail.com>
2022-03-10 22:58:34 +0900

    [update]newBox_jQuery_function_add

--- js/src/newBox.js
+++ js/src/newBox.js
@@ -51,12 +51,14 @@
     var listContent, modalImg;
     var dataLen = data.length;
 
-    // 함수
+    // 함수 ================================================================================
+    // 뉴박스 모달 리스트에 html코드를 넣어줄 때 쓰이는 함수
     var fnList = function(productName, imgNarr, price){
       return '<li class="new_product"><div class="product_name">'+productName+'</div><a href="#"><div class="product_image"><span class="blind">'+imgNarr+'</span></div><p>'+'&#8361;'+price+'</p></a></li>';
     };
 
-    // 기본실행
+    // 기본실행 ============================================================================
+    // 뉴박스 모달 리스트에 필요한 정보를 제이슨에서부터 불러와 넣어주는 기능
     for(var i=0; i < dataLen; i += 1){
       listContent = fnList(prData[i].product, prData[i].description, prData[i].price);
       modalUl.append(listContent);
@@ -65,32 +67,58 @@
     };
 
 
-    // 변형 변수
+    // 변형 변수 ===========================================================================
     var modalList = modalUl.children('li');
     var modalLink = modalList.children('a');
     var x = 0;
     var modalProduct = modalList.find('.product_name');
 
-    // 기본 실행
+
+    // 기본 실행 ===========================================================================
     // 모달리스트의 제품명은 마우스를 올리거나 포커스가 잡히지 않는 경우 보이지 않도록 기본적으로 숨겨놓음.
     modalProduct.hide();
 
+
     // 함수 ===========================================================================
+    // 모달 윈도우를 닫으면서 그 안 요소들에 담겨있던 정보들을 비워주는 함수
     var modalCloseFn = function(){
-      modalWinImg.empty();
-      modalDt.empty();
-      modalPrNum.empty();
-      modalPrNarr.empty(); 
-      modalPrice.empty();
+      var wantEmptyEl = [modalWinImg, modalDt, modalPrNum, modalPrNarr, modalPrice];
+      wantEmptyEl.forEach(function(el){
+        el.empty();
+      });
     }; // modalCloseFn()
 
-    // 이벤트
+    // 뉴박스의 모달리스트 이전버튼이 처음에는 없었다가 다음버튼을 눌렀을 때 다시 생기는 함수
+    var modalPrevBtnFn = function(){
+      if(x >= 0){
+        modalPrevBtn.hide();
+      }else{
+        modalPrevBtn.show();
+      }
+    };
+    modalPrevBtnFn();
+
+    // 뉴박스의 모달리스트 다음버튼 계속 있다가 리스트의 마지막이 됐을 때 사라지게 만드는 함수
+    var modalNextBtnFn = function(){
+      if(x > -2){
+        modalNextBtn.show();
+      }else if(x <= -2){
+        modalNextBtn.hide();
+      }
+    };
+    modalNextBtnFn();
+
+
+    // 이벤트 ===========================================================================
     // 모달 리스트 버튼 클릭 이벤트 ======================================================
     modalNextBtn.on('click', function(e){
       e.preventDefault();
       if(x > -2){
         x -= 1;
-        modalUl.animate({'marginLeft' : (100*x) + '%'});
+        modalUl.animate({'marginLeft' : (100*x) + '%'}, function(){
+          modalPrevBtnFn();
+          modalNextBtnFn();
+        });
       };
     });
 
@@ -98,31 +126,29 @@
       e.preventDefault();
       if(x < 0){
         x += 1;
-        modalUl.animate({'marginLeft' : (100*x) + '%'});
+        modalUl.animate({'marginLeft' : (100*x) + '%'}, function(){
+          modalPrevBtnFn();
+          modalNextBtnFn();
+        });
       };
     });
 
+    // var fnSlideMove = function(timeN){
+    //   var n = timeN;
+    //   var i = $(this).parent().index();
+    //   modalProduct.eq(i).stop().slideDown(timed/n);
+    // };  
+
+    
     // 모달 리스트 클릭 이벤트 ============================================================
-    // 모달 리스트에 마우스를 올렸을 때 이벤트 ------------------------
-    modalLink.on('mouseenter', function(){
+    // 모달 리스트에 마우스를 올렸을 때와 포커스 됐을 때 이벤트 ------------------------
+    modalLink.on('mouseenter focus', function(){
       var i = $(this).parent().index();
       modalProduct.eq(i).stop().slideDown(timed/4);
     });
 
-    // 모달 리스트에 마우스가 벗어났을 때 이벤트 ----------------------
-    modalLink.on('mouseleave', function(){
-      var i = $(this).parent().index();
-      modalProduct.eq(i).stop().slideUp(timed/3);
-    });
-
-    // 모달 리스트에 포커스가 됐을 때 이벤트 --------------------------
-    modalLink.on('focus', function(){
-      var i = $(this).parent().index();
-      modalProduct.eq(i).stop().slideDown(timed/4);
-    });
-
-    // 모달 리스트에 포커스가 벗어났을 때 이벤트 ----------------------
-    modalLink.on('blur', function(){
+    // 모달 리스트에 마우스가 벗어나거나 포커스가 벗어났을 때 이벤트 ----------------------
+    modalLink.on('mouseleave blur', function(){
       var i = $(this).parent().index();
       modalProduct.eq(i).stop().slideUp(timed/3);
     });
@@ -132,7 +158,7 @@
       e.preventDefault();
       var idx = $(this).parent().index();
       var prListData = prData[idx];
-
+      
       modalWinImg.css({'backgroundImage':'url('+imgUrl+prListData.image+')'});
       modalDt.append(prListData.product);
       modalPrNum.append(prListData.number);
